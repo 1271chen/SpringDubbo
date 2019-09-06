5f5fe6c2af6c5c3da9a18102c067e2acba416c2b
504031968@qq.com <chen134679>
2019-09-06 20:10:34 +0800

    22222

--- consumer/src/main/java/com/mooc/jiangzh/dubbo/springboot/Test.java
+++ consumer/src/main/java/com/mooc/jiangzh/dubbo/springboot/Test.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 public class Test {
 
+    private static String name="zhangsan";
+
     public static void main(String[] args) {
         HashMap map=new HashMap<>();
         Map<String,Object> map1=new HashMap<>();
